46b70bce5c1bdbb9ca89946c66ea3b9ee55a3951
Tony <anton.moskalenko@icloud.com>
2026-02-15 11:36:51 +0400

    header, first section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-import "../sass/style.scss";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "/src/sass/style.scss";
+
+try {
+    const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
