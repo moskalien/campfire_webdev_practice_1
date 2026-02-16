5e31142423ab4b3b5b9aac6601fc6022e0559909
busy <test@Mac-mini.local>
2026-02-16 22:26:24 +0400

    feat(results): add second section with swiper slider
    - configure navigation and pagination

--- src/js/script.js
+++ src/js/script.js
@@ -8,12 +8,35 @@ import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
 try {
-    const swiper = new Swiper(".mySwiper", {
+    const swiper = new Swiper(".promo__swiper", {
         spaceBetween: 30,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+            loop: true,
+        },
+        modules: [Navigation, Pagination],
+    });
+
+    const arrows = new Swiper(".results__swiper", {
+        spaceBetween: 30,
+        slidesPerView: 4,
+        loop: true,
+        hashNavigation: {
+            watchState: true,
+        },
+
+        navigation: {
+            nextEl: ".results__button-next",
+            prevEl: ".results__button-prev",
+        },
+        pagination: {
+            el: ".results__arrows-pagination",
+            clickable: true,
+            loop: true,
         },
         modules: [Navigation, Pagination],
     });
 } catch (e) { }
+
+
